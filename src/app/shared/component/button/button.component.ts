9d2c7071148858c07becd1af6648dc551329d15f
arksohail <arksohail@gmail.com>
2024-09-24 20:52:31 +0530

    Making Sense of Component Host Element

--- src/app/shared/component/button/button.component.ts
+++ src/app/shared/component/button/button.component.ts
@@ -1,7 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]. a[appButton]',
+  selector: 'button[appButton], a[appButton]',
   standalone: true,
   imports: [],
   template: `
@@ -11,31 +11,31 @@ import { Component, input } from '@angular/core';
     <ng-content select="icon" />
   `,
   styles: `
-  button {
-    display: inline-block;
-    padding: 0.65rem 1.35rem;
-    border-radius: 0.25rem;
-    font-size: 1rem;
-    text-align: center;
-    cursor: pointer;
-    background-color: #691ebe;
-    color: white;
-    border: none;
-  }
+    :host {
+      display: inline-block;
+      padding: 0.65rem 1.35rem;
+      border-radius: 0.25rem;
+      font-size: 1rem;
+      text-align: center;
+      cursor: pointer;
+      background-color: #691ebe;
+      color: white;
+      border: none;
+    }
 
-  button:hover {
-    background-color: #551b98;
-  }
+    :host:hover {
+      background-color: #551b98;
+    }
 
-  .icon {
-    display: inline-block;
-    margin-left: 0.5rem;
-    transition: transform 0.2s ease-in-out;
-  }
+    .icon {
+      display: inline-block;
+      margin-left: 0.5rem;
+      transition: transform 0.2s ease-in-out;
+    }
 
-  button:hover .icon {
-    transform: translateX(4px);
-  }
-  `
+    :host:hover .icon {
+      transform: translateX(4px);
+    }
+  `,
 })
 export class ButtonComponent { }
