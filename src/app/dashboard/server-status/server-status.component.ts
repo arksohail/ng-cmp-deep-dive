006ae30d20d4ce3a5cc061fd4ad82658b2adb43e
arksohail <arksohail@gmail.com>
2024-09-25 00:22:08 +0530

    Introducing the Component Lifecycle: ngOnInit

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,19 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
-  constructor() {
+  constructor() { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
     setInterval(() => {
       const rnd = Math.random();
-      console.log(rnd)
-      if(rnd < 0.5) {
-        this.currentStatus = 'online'              
-      } else if(rnd < 0.9) {
-        this.currentStatus = 'offline'            
+      // console.log(rnd)
+      if (rnd < 0.5) {
+        this.currentStatus = 'online'
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline'
       } else {
-        this.currentStatus = 'unknown'              
+        this.currentStatus = 'unknown'
       }
     }, 5000);
   }
